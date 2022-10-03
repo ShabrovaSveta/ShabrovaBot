da678f4c369fc773131d1f03d9486d8384e70c0a
ShabrovaSveta <svetashabrova@gmail.com>
2022-10-03 18:00:12 +0400

    Update app.py

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def get_price(message: telebot.types.Message):
     except Exception as e:
         bot.reply_to(message, f'Не удалось обработать команду\n {e}')
     else:
-        text = f'Цена {amount} {quote} в {base} = {total_base}'
+        text = f' {amount} {quote} в {base} = {total_base}'
         bot.send_message(message.chat.id, text)
 
 
