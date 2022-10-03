069f98c7e10e527c5d392ed2bcdeab39c8a99fd8
ShabrovaSveta <svetashabrova@gmail.com>
2022-10-03 17:51:47 +0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+TOKEN = '5647683836:AAGjJWF8ReH-pDakvx0GseRvuvHtG_vWoNM'
+
+keys = {
+    'рубль': 'RUB',
+    'евро': 'EUR',
+    'доллар': 'USD',
+    'юань':  'CNY',
+    'йен': 'JPY',
+    'франк': 'CHF'
+
+}
